4dd3712be40ea39e007b7af0e0c281af64f2cee0
teteuHD <114925463+teteuhd@users.noreply.github.com>
2022-10-06 20:26:16 -0300

    Add files via upload

---
+++ Site/atv_formas/script.js
@@ -0,0 +1,16 @@
+function quadrado(){
+    document.getElementById('forma').className = 'quadrado'
+}
+
+function circulo(){
+    document.getElementById('forma').className = 'circulo'
+}
+
+function retangulo(){
+    document.getElementById('forma').className = 'retangulo'
+}
+
+function estrela(){
+    document.getElementById('forma').className = 'estrela'
+}
+
